1410a039f0edc03a551f2458692406b0ae947e49
Zhu <339357762@qq.com>
2015-02-03 14:23:11 +0800

    添加用户实体

---
+++ src/main/java/com/zhu/prototype/entity/User.java
@@ -0,0 +1,43 @@
+package com.zhu.prototype.entity;
+
+import java.util.Date;
+
+public class User {
+	private String username;
+	private String password;
+	private Date birthday;
+	private Double height;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public Double getHeight() {
+		return height;
+	}
+
+	public void setHeight(Double height) {
+		this.height = height;
+	}
+
+}
