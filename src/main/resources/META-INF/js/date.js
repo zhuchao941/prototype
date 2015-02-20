9064e682e9cd2825cd8603a7f27ad8e7e7e97fc7
zhu <zhu339357762@gmail.com>
2015-02-20 21:13:26 +0800

    处理日期格式化的 工具文件

---
+++ src/main/resources/META-INF/js/date.js
@@ -0,0 +1,52 @@
+// 对Date的扩展，将 Date 转化为指定格式的String   
+// 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，   
+// 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)   
+// 例子：   
+// (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423   
+// (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18   
+Date.prototype.format = function(fmt) { // author: meizz
+	var o = {
+		"M+" : this.getMonth() + 1, // 月份
+		"d+" : this.getDate(), // 日
+		"h+" : this.getHours(), // 小时
+		"m+" : this.getMinutes(), // 分
+		"s+" : this.getSeconds(), // 秒
+		"q+" : Math.floor((this.getMonth() + 3) / 3), // 季度
+		"S" : this.getMilliseconds()
+	// 毫秒
+	};
+	if (/(y+)/.test(fmt))
+		fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "")
+				.substr(4 - RegExp.$1.length));
+	for ( var k in o)
+		if (new RegExp("(" + k + ")").test(fmt))
+			fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k])
+					: (("00" + o[k]).substr(("" + o[k]).length)));
+	return fmt;
+}
+
+//把时间格式字符串转化为时间
+//如下格式
+//2006
+//2006-01
+//2006-01-01
+//2006-01-01 12
+//2006-01-01 12:12
+//2006-01-01 12:12:12
+Date.parseDate = function(dateStr) {
+  var regexDT = /(\d{4})-?(\d{2})?-?(\d{2})?\s?(\d{2})?:?(\d{2})?:?(\d{2})?/g;
+  var matchs = regexDT.exec(dateStr);
+  var date = new Array();
+  for (var i = 1; i < matchs.length; i++) {
+      if (matchs[i]!=undefined) {
+          date[i] = matchs[i];
+      } else {
+          if (i<=3) {
+              date[i] = '01';
+          } else {
+              date[i] = '00';
+          }
+      }
+  }
+  return new Date(date[1], date[2]-1, date[3], date[4], date[5],date[6]);
+}
