39aabfc1a7e3f2fe68cd0a4d7499887bcba50402
zhu <zhu339357762@gmail.com>
2015-02-20 21:05:54 +0800

    虎扑抓取新闻的一些常量

---
+++ src/main/java/com/zhu/prototype/task/grab/support/constant/HoopConstant.java
@@ -0,0 +1,8 @@
+package com.zhu.prototype.task.grab.support.constant;
+
+public class HoopConstant {
+
+	public final static String NEWS_LIST = "http://voice.hupu.com/nba/newslist"; // NBA新闻
+	public final static String NBA_CENTER = "http://g.hupu.com/nba"; // 比赛中心
+	public final static String NBA_INDEX = "http://nba.hupu.com"; // 比赛中心
+}
