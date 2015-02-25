7bc56e014971a42441241c6a48a009e1d4070cd2
zhu <zhu339357762@gmail.com>
2015-02-25 09:56:59 +0800

    添加一些对用户的操作接口

--- src/main/java/com/zhu/prototype/web/UserController.java
+++ src/main/java/com/zhu/prototype/web/UserController.java
@@ -9,16 +9,21 @@ import org.springframework.validation.Validator;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.zhu.prototype.dto.UserDTO;
 import com.zhu.prototype.dto.UserPreferences;
 import com.zhu.prototype.entity.User;
+import com.zhu.prototype.service.UserService;
 
 @Controller
 public class UserController extends BaseController {
 
 	@Resource
 	private Validator userValidator;
+
+	@Resource
+	private UserService userService;
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String showLoginPage() {
@@ -28,37 +33,45 @@ public class UserController extends BaseController {
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String submitLoginForm(User user, HttpSession session) {
 		if ("success".equals(validateUser(user, session))) {
-			return "news/newsList";
+			return "redirect:news/newsList";
 		}
 		return showLoginPage();
 	}
 
 	private String validateUser(User user, HttpSession session) {
-		if (user.getUsername().equals("zhu")
-				&& user.getPassword().equals("1234")) {
-			UserPreferences preferences = new UserPreferences();
-			preferences.setUsername(user.getUsername());
-			session.setAttribute("userPreferences", preferences);
+		if (userService.validateUser(user)) {
+			doLogin(user, session);
 			return "success";
 		}
 		return "fail";
 	}
 
+	private void doLogin(User user, HttpSession session) {
+		UserPreferences preferences = new UserPreferences();
+		preferences.setUsername(user.getUsername());
+		session.setAttribute("userPreferences", preferences);
+	}
+
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
-	public String showRegisterPage() {
-		return "user/register";
+	public ModelAndView showRegisterPage() {
+		ModelAndView mv = new ModelAndView("user/login");
+		mv.addObject("type", "register");
+		return mv;
 	}
 
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public String submitRegisterForm(@ModelAttribute("user") UserDTO user,
-			Errors errors) {
+	public ModelAndView submitRegisterForm(
+			@ModelAttribute("user") UserDTO user, Errors errors,
+			HttpSession session) {
 
 		userValidator.validate(user, errors);
 		if (errors.hasErrors()) {
 			System.out.println(errors);
 			return showRegisterPage();
 		}
-		return "user/register-success";
+		userService.register(user);
+		doLogin(user, session);
+		return new ModelAndView("redirect:news/newsList");
 	}
 
 	@ModelAttribute("user")
