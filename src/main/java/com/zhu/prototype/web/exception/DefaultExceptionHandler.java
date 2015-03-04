077b3fb4110de1cc31610d47de55da335b11a46c
Zhu <339357762@qq.com>
2015-03-04 17:45:03 +0800

    统一处理异常类

--- src/main/java/com/zhu/prototype/web/exception/DefaultExceptionHandler.java
+++ src/main/java/com/zhu/prototype/web/exception/DefaultExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.zhu.prototype.web.exception;
 
-import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authz.UnauthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,18 +12,20 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @ControllerAdvice
 public class DefaultExceptionHandler {
-    /**
-     * 没有权限 异常
-     * <p/>
-     * 后续根据不同的需求定制即可
-     */
-    @ExceptionHandler({AuthenticationException.class})
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ModelAndView processUnauthenticatedException(NativeWebRequest request, UnauthorizedException e) {
-        ModelAndView mv = new ModelAndView();
-        mv.addObject("exception", e);
-        mv.setViewName("unauthorized");
-        return mv;
-    }
+	/**
+	 * 没有权限 异常
+	 * <p/>
+	 * 后续根据不同的需求定制即可
+	 */
+	@ExceptionHandler( { UnauthorizedException.class })
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
+	public ModelAndView processUnauthenticatedException(
+			NativeWebRequest request, UnauthorizedException e) {
+		ModelAndView mv = new ModelAndView();
 
+		mv.addObject("contextPath", request.getContextPath());
+		mv.addObject("exception", e);
+		mv.setViewName("special/unauthorized");
+		return mv;
+	}
 }
