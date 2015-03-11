f250c6641ccb147428a871acfcd055cb08c50c15
Zhu <339357762@qq.com>
2015-03-11 17:44:15 +0800

    处理新闻中没有p标签的情况

--- src/main/java/com/zhu/prototype/utils/StringUtils.java
+++ src/main/java/com/zhu/prototype/utils/StringUtils.java
@@ -5,8 +5,13 @@ public class StringUtils extends org.apache.commons.lang.StringUtils {
 	private static String htmlReg = "<([^>]*)>";
 
 	public static String buildDigest(String content) {
-		content = content.substring(content.indexOf("<p>") + "<p>".length(),
-				content.indexOf("</p>"));
+		if(content.contains("<p>")){
+			content = content.substring(content.indexOf("<p>") + "<p>".length(),
+					content.indexOf("</p>"));
+		}else{
+			content = content.substring(content.indexOf("<div>") + "<div>".length(),
+					content.indexOf("</div>"));
+		}
 		// 去除所有标签，防止恰好截到标签
 		return content.replaceAll(htmlReg, "") + "……";
 	}
