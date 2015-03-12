7809b9b3702e24b9df66dbabaf962c07c5235d93
Zhu <339357762@qq.com>
2015-03-12 17:41:16 +0800

    UserPreferences提到service中，方便多地方使用

--- src/main/java/com/zhu/prototype/service/UserService.java
+++ src/main/java/com/zhu/prototype/service/UserService.java
@@ -1,11 +1,12 @@
 package com.zhu.prototype.service;
 
+import com.zhu.prototype.dto.UserPreferences;
 import com.zhu.prototype.entity.User;
 
 public interface UserService {
 	public boolean register(User user);
 
-	public boolean validateUser(User user);
-
 	public User getUserByUsername(String username);
+	
+	public UserPreferences getUserPreferences(User user);
 }
--- src/main/java/com/zhu/prototype/service/impl/UserServiceImpl.java
+++ src/main/java/com/zhu/prototype/service/impl/UserServiceImpl.java
@@ -1,17 +1,14 @@
 package com.zhu.prototype.service.impl;
 
-import java.util.List;
-
 import javax.annotation.Resource;
 
 import org.apache.shiro.authc.credential.PasswordService;
 import org.springframework.stereotype.Service;
 
 import com.zhu.prototype.dao.UserMapper;
+import com.zhu.prototype.dto.UserPreferences;
 import com.zhu.prototype.entity.User;
-import com.zhu.prototype.entity.UserExample;
 import com.zhu.prototype.service.UserService;
-import com.zhu.prototype.utils.CollectionUtils;
 
 @Service("userService")
 public class UserServiceImpl implements UserService {
@@ -29,25 +26,17 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	public boolean validateUser(User user) {
-		if (user == null || user.getUsername() == null
-				|| user.getPassword() == null) {
-			return false;
-		}
-		UserExample example = new UserExample();
-		example.createCriteria().andUsernameEqualTo(user.getUsername())
-				.andPasswordEqualTo(user.getPassword());
-		List<User> userList = userMapper.selectByExample(example);
-		if (CollectionUtils.isEmpty(userList)) {
-			return false;
-		}
-		user = userList.get(0);
-		return true;
-	}
-
-	@Override
 	public User getUserByUsername(String username) {
 		return userMapper.selectByPrimaryKey(username);
 	}
+
+	@Override
+	public UserPreferences getUserPreferences(User user) {
+		UserPreferences preferences = new UserPreferences();
+		preferences.setUsername(user.getUsername());
+		return preferences;
+	}
+	
+	
 
 }
